398b04c59f74323fdd5860dedacfc470f66ad837
unnati2000 <unnatibamania8@gmail.com>
2021-09-30 13:06:35 +0530

    Connect database

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
-const dev = process.env.NODE_ENV !== "production";
+require("dotenv").config({ path: "./config.env" });
+const connectDB = require("./utilsServer/connectDB");
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+connectDB();
+
+console.log(process.env.MONGO_URI);
 
 nextApp.prepare().then(() => {
   app.all("*", (req, res) => handle(req, res));
