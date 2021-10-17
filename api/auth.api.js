dce930ed70db9b7d99fc8738577b1ff3b0073114
unnati2000 <unnatibamania8@gmail.com>
2021-10-17 22:16:30 +0530

    Forgot password backend

--- api/auth.api.js
+++ api/auth.api.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 const router = express.Router();
 
 const User = require("../models/user.models");
 const auth = require("../middleware/auth.middleware");
+
+const sendEmail = require("../utils-server/sendEmail");
 
 router.get("/", auth, async (req, res) => {
   try {
@@ -54,6 +57,48 @@ router.post("/", async (req, res) => {
       if (err) throw err;
       res.status(200).json({ token });
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+router.post("/forgot-password", async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    user.resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+    user.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    const resetUrl = `${req.protocol}://${req.get(
+      "host"
+    )}/reset-password/${resetToken}`;
+
+    try {
+      await sendEmail({
+        to: user.email,
+        subject: "Docbook - Reset Password",
+        html: `<p>${resetUrl}</p>`,
+      });
+    } catch (error) {
+      console.log(error);
+      user.resetPasswordToken = undefined;
+      await user.save();
+      return res.status(500).json({ msg: "Error sending verification email" });
+    }
+
+    await user.save();
+    res.status(200).json({ msg: "Email sent" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ msg: "Server error" });
