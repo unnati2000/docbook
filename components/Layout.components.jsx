3b8b3cecd4c542513576d982d8706478a746354c
unnati2000 <unnatibamania8@gmail.com>
2021-11-22 10:00:18 +0530

    Navbar layout change

--- components/Layout.components.jsx
+++ components/Layout.components.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import Navbar from "./Navbar.component";
+import Searchbar from "./Searchbar.component";
 
-const Layout = ({ children, user }) => {
+const Layout = ({ user, children }) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <div>
-      {user && <Navbar user={user} />}
-      {children}
+    <div className="h-screen bg-gray-50 flex overflow-hidden">
+      <Navbar
+        mobileMenuOpen={mobileMenuOpen}
+        setMobileMenuOpen={setMobileMenuOpen}
+      />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Searchbar
+          mobileMenuOpen={mobileMenuOpen}
+          setMobileMenuOpen={setMobileMenuOpen}
+        />
+
+        <main>{children}</main>
+      </div>
     </div>
   );
 };
--- components/Navbar.component.jsx
+++ components/Navbar.component.jsx
@@ -1,21 +1,168 @@
-import { AiFillHome, AiOutlineLogout } from "react-icons/ai";
-import { MdDashboard } from "react-icons/md";
-import { logoutUser } from "../utils/auth.utils";
+import { Fragment } from "react";
+import {
+  CogIcon,
+  CollectionIcon,
+  HomeIcon,
+  PhotographIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+  XIcon,
+} from "@heroicons/react/outline";
 
-const Navbar = ({ user }) => {
+import { Dialog, Menu, Transition } from "@headlessui/react";
+
+const sidebarNavigation = [
+  { name: "Home", href: "#", icon: HomeIcon, current: false },
+  { name: "All Files", href: "#", icon: ViewGridIcon, current: false },
+  { name: "Photos", href: "#", icon: PhotographIcon, current: true },
+  { name: "Shared", href: "#", icon: UserGroupIcon, current: false },
+  { name: "Albums", href: "#", icon: CollectionIcon, current: false },
+  { name: "Settings", href: "#", icon: CogIcon, current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Navbar = ({ user, mobileMenuOpen, setMobileMenuOpen }) => {
   return (
-    <nav className="flex justify-between items-center shadow-md">
-      <div className="p-2">
-        <img className="h-12 w-12" alt="logo" src="/images/logo.png" />
+    <>
+      <div className="hidden w-28 bg-blue-700 overflow-y-auto md:block">
+        <div className="w-full py-6 flex flex-col items-center">
+          <div className="flex-shrink-0 flex items-center">
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+              alt="Workflow"
+            />
+          </div>
+          <div className="flex-1 mt-6 w-full px-2 space-y-1">
+            {sidebarNavigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className={classNames(
+                  item.current
+                    ? "bg-blue-800 text-white"
+                    : "text-indigo-100 hover:bg-blue-800 hover:text-white",
+                  "group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium"
+                )}
+                aria-current={item.current ? "page" : undefined}
+              >
+                <item.icon
+                  className={classNames(
+                    item.current
+                      ? "text-white"
+                      : "text-indigo-300 group-hover:text-white",
+                    "h-6 w-6"
+                  )}
+                  aria-hidden="true"
+                />
+                <span className="mt-2">{item.name}</span>
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="flex space-x-4 items-center mx-6">
-        <AiFillHome className="text-2xl text-blue-600" />
-        <MdDashboard className="text-2xl text-blue-600" />
-        <a onClick={logoutUser}>
-          <AiOutlineLogout className="text-2xl text-blue-600" />
-        </a>
-      </div>
-    </nav>
+
+      {/* Mobile menu */}
+      <Transition.Root show={mobileMenuOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          static
+          className="md:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-40 flex">
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+            </Transition.Child>
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
+            >
+              <div className="relative max-w-xs w-full bg-indigo-700 pt-5 pb-4 flex-1 flex flex-col">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-in-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in-out duration-300"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="absolute top-1 right-0 -mr-14 p-1">
+                    <button
+                      type="button"
+                      className="h-12 w-12 rounded-full flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-white"
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      <XIcon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                      <span className="sr-only">Close sidebar</span>
+                    </button>
+                  </div>
+                </Transition.Child>
+                <div className="flex-shrink-0 px-4 flex items-center">
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="mt-5 flex-1 h-0 px-2 overflow-y-auto">
+                  <nav className="h-full flex flex-col">
+                    <div className="space-y-1">
+                      {sidebarNavigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-indigo-800 text-white"
+                              : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
+                            "group py-2 px-3 rounded-md flex items-center text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          <item.icon
+                            className={classNames(
+                              item.current
+                                ? "text-white"
+                                : "text-indigo-300 group-hover:text-white",
+                              "mr-3 h-6 w-6"
+                            )}
+                            aria-hidden="true"
+                          />
+                          <span>{item.name}</span>
+                        </a>
+                      ))}
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </Transition.Child>
+            <div className="flex-shrink-0 w-14" aria-hidden="true"></div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
   );
 };
 
