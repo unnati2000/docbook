1cbbac5ffe7838a97804e136219f2caf7d426e26
Unnati Bamania <unnatibamania8@gmail.com>
2022-01-27 13:42:54 +0530

    appointment model

---
+++ models/appointment.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    timeSlot: {
+      type: String,
+      required: true,
+    },
+    fee: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    day: {
+      type: String,
+      required: true,
+    },
+    confirmed: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", appointmentSchema);
