326256dc994bb8f7cda13378f1131c3784678be5
Unnati Bamania <unnatibamania8@gmail.com>
2022-02-02 00:27:59 +0530

    Doctor details update route complete

--- components/settings/DoctorDetails.component.jsx
+++ components/settings/DoctorDetails.component.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useMutation } from "react-query";
 import { useRouter } from "next/router";
+import cookie from "js-cookie";
 import { toast } from "react-toastify";
 import baseURL from "../../utils/baseURL";
 import TimingsForm from "../doctor-details/TimingsForm.component";
@@ -11,7 +12,7 @@ const DoctorDetails = ({ user }) => {
 
   const [initialFee, setInitialFee] = useState(user?.doctor?.initialFee);
 
-  const [description, setDescriptipn] = useState(user?.doctor?.description);
+  const [description, setDescription] = useState(user?.doctor?.description);
 
   const [monday, setMonday] = useState({
     from: user?.doctor?.timings?.monday?.from,
@@ -65,43 +66,95 @@ const DoctorDetails = ({ user }) => {
       friday,
       saturday,
       initialFee,
+      description,
     }) => {
-      const { data } = await axios.post(`${baseURL}/api/doctor`, {
-        sunday,
-        monday,
-        tuesday,
-        wednesday,
-        thursday,
-        friday,
-        saturday,
-        initialFee,
-      });
+      const { data } = await axios.post(
+        `${baseURL}/api/doctor`,
+        {
+          sunday,
+          monday,
+          tuesday,
+          wednesday,
+          thursday,
+          friday,
+          saturday,
+          initialFee,
+          description,
+        },
+        {
+          headers: {
+            Authorization: cookie.get("token"),
+          },
+        }
+      );
       return data;
     }
   );
 
   const onSubmit = async (e) => {
     e.preventDefault();
+
+    // sunday
     if (sunday.markAsHoliday === true) {
       setSunday({ ...sunday, from: "", to: "" });
+    } else if (sunday.from.split(":")[0] === "00") {
+      sunday.from = `12:${sunday.from.split(":")[1]}}`;
+    } else if (sunday.to.split(":")[0] === "00") {
+      sunday.to = `12:${sunday.to.split(":")[1]}}`;
     }
+
+    // monday
     if (monday.markAsHoliday === true) {
       setMonday({ ...monday, from: "", to: "" });
+    } else if (monday.from.split(":")[0] === "00") {
+      monday.from = `12:${monday.from.split(":")[1]}}`;
+    } else if (monday.to.split(":")[0] === "00") {
+      monday.to = `12:${monday.to.split(":")[1]}}`;
     }
+
+    // tuesday
     if (tuesday.markAsHoliday === true) {
       setTuesday({ ...tuesday, from: "", to: "" });
+    } else if (tuesday.from.split(":")[0] === "00") {
+      tuesday.from = `12:${tuesday.from.split(":")[1]}}`;
+    } else if (tuesday.to.split(":")[0] === "00") {
+      tuesday.to = `12:${tuesday.to.split(":")[1]}}`;
     }
+
+    // wednesday
     if (wednesday.markAsHoliday === true) {
       setWednesday({ ...wednesday, from: "", to: "" });
+    } else if (wednesday.from.split(":")[0] === "00") {
+      wednesday.from = `12:${wednesday.from.split(":")[1]}}`;
+    } else if (wednesday.to.split(":")[0] === "00") {
+      wednesday.to = `12:${wednesday.to.split(":")[1]}}`;
     }
+
+    // Thursday
     if (thursday.markAsHoliday === true) {
       setThursday({ ...thursday, from: "", to: "" });
+    } else if (thursday.from.split(":")[0] === "00") {
+      thursday.from = `12:${thursday.from.split(":")[1]}}`;
+    } else if (thursday.to.split(":")[0] === "00") {
+      thursday.to = `12:${thursday.to.split(":")[1]}}`;
     }
+
+    // Friday
     if (friday.markAsHoliday === true) {
       setFriday({ ...friday, from: "", to: "" });
+    } else if (friday.from.split(":")[0] === "00") {
+      friday.from = `12:${friday.from.split(":")[1]}}`;
+    } else if (friday.to.split(":")[0] === "00") {
+      friday.to = `12:${friday.to.split(":")[1]}}`;
     }
+
+    // Saturday
     if (saturday.markAsHoliday === true) {
       setSaturday({ ...saturday, from: "", to: "" });
+    } else if (saturday.from.split(":")[0] === "00") {
+      saturday.from = `12:${saturday.from.split(":")[1]}}`;
+    } else if (saturday.to.split(":")[0] === "00") {
+      saturday.to = `12:${saturday.to.split(":")[1]}}`;
     }
 
     const data = await mutation.mutateAsync({
@@ -113,6 +166,7 @@ const DoctorDetails = ({ user }) => {
       friday,
       saturday,
       initialFee,
+      description,
     });
     toast.success(data.msg);
     router.push("/");
