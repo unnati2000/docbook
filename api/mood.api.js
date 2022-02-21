175f07e2505a423989b00838bfa96c4d2bfad3f1
Unnati Bamania <unnatibamania8@gmail.com>
2022-02-21 19:31:35 +0530

    Mood api

--- api/mood.api.js
+++ api/mood.api.js
@@ -1,4 +1,37 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth.middleware");
 
+router.post("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    let mood = await Mood.findOne({ user: req.userId });
+    if (!mood) {
+      mood = new Modal({
+        user: req.userId,
+        moods: [
+          {
+            moodType: req.body.moodType,
+            moodScore: req.body.moodScore,
+            moodDate: req.body.moodDate,
+            description: req.body.description,
+          },
+        ],
+      });
+    } else {
+      mood = mood.findOneAndUpdate(
+        { user: req.userId },
+        { $push: { moods: req.body } },
+        { new: true }
+      );
+      return res.json({ msg: "Recorded successfully", mood });
+    }
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
 module.exports = router;
